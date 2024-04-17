2e3a2293a7b36518bd5ef5611ba297d68ef5830e
Denis <denisdamse@gmail.com>
2024-04-17 09:45:22 +0300

    done

--- src/App.java
+++ src/App.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         ArrayList<String> stringArray = new ArrayList<String>();
         stringArray.add("Denis");
         stringArray.add("Andrei");
